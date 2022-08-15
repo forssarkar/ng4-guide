38e426830681513be9342ae9180abc3db18c4153
forssarkar <forssarkar@gmail.com>
2022-08-15 18:57:34 +0530

    Adding Nav Bar, Receipe Model, Adding Conent Receipes Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
 import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail
     ShoppingListComponent,
     ShoppingEditComponent,
     ReceipeListComponent,
-    ReceipeDetailComponent
+    ReceipeDetailComponent,
+    ReceipeItemComponent
   ],
   imports: [
     BrowserModule,
--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Receipe} from '../receipe.model'
+
 @Component({
   selector: 'app-receipe-list',
   templateUrl: './receipe-list.component.html',
   styleUrls: ['./receipe-list.component.css']
 })
 export class ReceipeListComponent implements OnInit {
+
+      receipes:Receipe []= [
+        new Receipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+       ];
 
   constructor() { }
 
