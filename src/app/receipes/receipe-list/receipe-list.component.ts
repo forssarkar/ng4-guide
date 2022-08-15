afaab541e8a1b9fb2c87ff6defeecc448ec6322d
forssarkar <forssarkar@gmail.com>
2022-08-15 20:04:57 +0530

    Lesson 03 : List of Receipe using ngFor, Display Receipe Details, Creation of Shopping List

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -10,6 +10,7 @@ import {Receipe} from '../receipe.model'
 export class ReceipeListComponent implements OnInit {
 
       receipes:Receipe []= [
+        new Receipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
         new Receipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
        ];
 
