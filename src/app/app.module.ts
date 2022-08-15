f26d486ebbe1a78a937bb838b4ec5755603f513d
forssarkar <forssarkar@gmail.com>
2022-08-15 13:46:15 +0530

    Project Setup with Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent
   ],
   imports: [
     BrowserModule,
